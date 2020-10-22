ee8003f0fdd2e394b26f05dce8834194fd1ff788
ChristyTropila <tropila04@gmail.com>
2020-10-21 23:58:24 -0400

    final push wednesday

--- app/serializers/bucket_serializer.rb
+++ app/serializers/bucket_serializer.rb
@@ -1,5 +1,5 @@
 class BucketSerializer < ActiveModel::Serializer
-    attributes :id, :user_id, :trick_treat_id
+    attributes :id, :user_id, :trick_treat_id, :getName
 
  belongs_to :trick_treat
     # belongs_to:  user
